47af3f1e20257771a0693f4b934cb22482e835cf
陽生 渡邊 <2250150@ecc.ac.jp>
2026-05-08 16:42:56 +0900

    鏡反射

--- Source/Mirror.cpp
+++ Source/Mirror.cpp
@@ -11,7 +11,7 @@ Mirror::Mirror()
     angle = { 0.0f, 0.0f, 0.0f };
     scale = { 1.0f, 1.0f, 1.0f };
 
-    RayHitType type = RayHitType::reflection;
+    type = RayHitType::reflection;
 
     UpdateTransform();
 }
