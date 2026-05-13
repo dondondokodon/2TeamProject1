8a36654bca1955923ff78e1562eace5d81516e67
dondondokodon <dokodon11@gmail.com>
2026-05-13 16:44:53 +0900

    基底クラスにもっていっていいかも ステージ切り替えれるようにする

--- Source/StageData2.cpp
+++ Source/StageData2.cpp
@@ -9,7 +9,7 @@ StageData2::StageData2()
 {
 	//ステージオブジェクトの配置データを追加
 	//オブジェクトの種類、位置、回転、大きさ、レーザーの向き（レーザー以外は無視される）
-	objects.push_back(std::make_unique<ObjectData>(ObjectData{ ObjectType::Laser,{   7.5f, 1.0f,  25.0f },{ 0.0f , 0.0f , 0.0f },{ 0.5f,0.5f,0.5f },{ 0.0f, 0.0f, -1.0f} }));
+	/*objects.push_back(std::make_unique<ObjectData>(ObjectData{ ObjectType::Laser,{   7.5f, 1.0f,  25.0f },{ 0.0f , 0.0f , 0.0f },{ 0.5f,0.5f,0.5f },{ 0.0f, 0.0f, -1.0f} }));
 	objects.push_back(std::make_unique<ObjectData>(ObjectData{ ObjectType::Laser,{  -7.5f, 1.0f,  25.0f },{ 0.0f , 0.0f , 0.0f },{ 0.5f,0.5f,0.5f },{ 0.0f, 0.0f, -1.0f} }));
 	objects.push_back(std::make_unique<ObjectData>(ObjectData{ ObjectType::Laser,{  25.0f, 1.0f,   6.0f },{ 0.0f , 0.0f , 0.0f },{ 0.5f,0.5f,0.5f },{-1.0f, 0.0f,  0.0f} }));
 	objects.push_back(std::make_unique<ObjectData>(ObjectData{ ObjectType::Laser,{ -25.0f, 1.0f,  -7.0f },{ 0.0f , 0.0f , 0.0f },{ 0.5f,0.5f,0.5f },{ 1.0f, 0.0f,  0.0f} }));
@@ -22,7 +22,7 @@ StageData2::StageData2()
 	objects.push_back(std::make_unique<ObjectData>(ObjectData{ ObjectType::Box,{  11.5f, 0.5f, -6.5f },{ 0.0f, 0.0f, 0.0f},{ 1.0f, 1.0f, 1.0f } }));
 	objects.push_back(std::make_unique<ObjectData>(ObjectData{ ObjectType::Mirror,{  -2.5f, 1.2f, 3.0f },{ 0.0f, 0.0f, 0.0f},{ 0.5f, 0.5f, 0.5f } }));
 	objects.push_back(std::make_unique<ObjectData>(ObjectData{ ObjectType::Mirror,{  4.5f, 1.2f, 0.5f },{ 0.0f, 0.0f, 0.0f},{ 0.5f, 0.5f, 0.5f } }));
-	objects.push_back(std::make_unique<ObjectData>(ObjectData{ ObjectType::Goal,{-1.0f, 3.0f,  17.0f },{ 0.0f, 0.0f, 0.0f},{ 0.01f, 0.01f, 0.01f } }));
+	objects.push_back(std::make_unique<ObjectData>(ObjectData{ ObjectType::Goal,{-1.0f, 3.0f,  17.0f },{ 0.0f, 0.0f, 0.0f},{ 0.01f, 0.01f, 0.01f } }));*/
 
 	int a;
 	////自分自身のオブジェクト（ステージ）
