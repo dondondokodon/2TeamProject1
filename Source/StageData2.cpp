70f72bc648843cb44d3e0f8bb0b082b8cd006479
dondondokodon <dokodon11@gmail.com>
2026-05-16 16:17:23 +0900

    ステージ差し替え

--- Source/StageData2.cpp
+++ Source/StageData2.cpp
@@ -1,6 +1,6 @@
 ﻿#include "StageData2.h"
 StageData2::StageData2():StageData("Data/Json/stage2.json")
 {
-	MyStage =std::make_unique<Stage>("Data/Model/Stage/tutorial_stage.mdl");
-	MyStage->SetScale({ 0.3f,0.3f,0.3f });
+	MyStage =std::make_unique<Stage>("Data/Model/Stage/stage_tutorial.mdl");
+	MyStage->SetScale({1.0f,1.0f,1.0f });
 }
