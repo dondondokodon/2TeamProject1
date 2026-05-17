7aa2a56cab11bc9081bae9718bfc97d75f92774e
dondondokodon <dokodon11@gmail.com>
2026-05-17 20:02:02 +0900

    たぶんステージ数増やせた

--- Source/StageData2.cpp
+++ Source/StageData2.cpp
@@ -4,3 +4,15 @@ StageData2::StageData2():StageData("Data/Json/stage2.json")
 	MyStage =std::make_unique<Stage>("Data/Model/Stage/stage_tutorial.mdl");
 	MyStage->SetScale({1.0f,1.0f,1.0f });
 }
+
+StageData3::StageData3() :StageData("Data/Json/stage3.json")
+{
+	MyStage = std::make_unique<Stage>("Data/Model/Stage/stage_tutorial.mdl");
+	MyStage->SetScale({ 1.0f,1.0f,1.0f });
+}
+
+StageData4::StageData4() :StageData("Data/Json/stage4.json")
+{
+	MyStage = std::make_unique<Stage>("Data/Model/Stage/stage_tutorial.mdl");
+	MyStage->SetScale({ 1.0f,1.0f,1.0f });
+}
