dbe0e09e19ae0238102373563b9e01b7df1c19d7
dondondokodon <dokodon11@gmail.com>
2026-05-17 21:36:54 +0900

    ステージ差し替えた

--- Source/StageData2.cpp
+++ Source/StageData2.cpp
@@ -7,12 +7,12 @@ StageData2::StageData2():StageData("Data/Json/stage2.json")
 
 StageData3::StageData3() :StageData("Data/Json/stage3.json")
 {
-	MyStage = std::make_unique<Stage>("Data/Model/Stage/stage_tutorial.mdl");
+	MyStage = std::make_unique<Stage>("Data/Model/Stage/main_stage.mdl");
 	MyStage->SetScale({ 1.0f,1.0f,1.0f });
 }
 
 StageData4::StageData4() :StageData("Data/Json/stage4.json")
 {
-	MyStage = std::make_unique<Stage>("Data/Model/Stage/stage_tutorial.mdl");
+	MyStage = std::make_unique<Stage>("Data/Model/Stage/main_stage.mdl");
 	MyStage->SetScale({ 1.0f,1.0f,1.0f });
 }
