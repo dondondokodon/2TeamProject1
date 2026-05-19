29a2930553010259baaf33435a807ae2bdc547c3
dondondokodon <dokodon11@gmail.com>
2026-05-19 23:17:35 +0900

    マネージャー作ってるとこ

---
+++ Source/AudioManager.cpp
@@ -0,0 +1,6 @@
+﻿#include "AudioManager.h"
+
+AudioManager::AudioManager()
+{
+	audios.push_back(std::make_unique<AudioSource>("Data/"));
+}
---
+++ Source/AudioManager.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "System/AudioSource.h"
+#include <vector>
+#include <memory>
+
+class AudioManager
+{
+	AudioManager();
+	~AudioManager() { Clear(); };
+
+public:
+	static AudioManager* Instance()
+	{
+		static AudioManager instance;
+		return &instance;
+	}
+
+	//更新処理
+	void Update(float elapsedTime);
+
+	//ステージオブジェクト全削除
+	void Clear();
+
+private:
+	std::vector<std::unique_ptr<AudioSource>> audios;
+};
+
