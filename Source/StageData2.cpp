c6343d7ced61a9d7fb16d7995bd5fa200c20ccb2
dondondokodon <dokodon11@gmail.com>
2026-05-21 11:28:34 +0900

    ステージの床変えた

--- Source/StageData2.cpp
+++ Source/StageData2.cpp
@@ -1,9 +1,9 @@
 ﻿#include "StageData2.h"
 StageData2::StageData2():StageData("Data/Json/stage2.json")
 {
-	MyStage =std::make_unique<Stage>("Data/Model/Stage/stage_tutorial.mdl");
+	MyStage = std::make_unique<Stage>("Data/Model/Stage/tutorial_wall.mdl");
 	MyStage->SetScale({1.0f,1.0f,1.0f });
-	StageFloor = std::make_unique<Stage>("Data/Model/Stage/main_stage_floor.mdl");
+	StageFloor = std::make_unique<Stage>("Data/Model/Stage/tutorial_floor.mdl");
 }
 
 StageData3::StageData3() :StageData("Data/Json/stage3.json")
